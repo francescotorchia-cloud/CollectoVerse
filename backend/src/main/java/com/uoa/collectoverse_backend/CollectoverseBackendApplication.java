24dcffaefcfe2ba2b547b338a772e23f4ac1309b
Francesco Torchia <francesco.torchia@studenti.unicam.it>
2026-01-21 17:47:47 +0100

    Struttura iniziale della repo : backend e frontend

---
+++ backend/src/main/java/com/uoa/collectoverse_backend/CollectoverseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uoa.collectoverse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectoverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectoverseBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/uoa/collectoverse_backend/CollectoverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uoa.collectoverse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectoverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
